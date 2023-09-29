fd692b5fd31417f93bc3067c43d7d84cc0188904
Hayden Kerr <10897906@my.uvu.edu>
2023-09-28 19:42:16 -0600

    AnimStates script and animation states in Unity
    Started working on the script that will control the different animation states but am still having trouble getting the animations to play through player input.

---
+++ Unity/Unit 1 Project/Assets/Scripts/AnimStatesBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class AnimStatesBehaviour : StateMachineBehaviour
+{
+  [SerializeField] private UnityEvent walkEvent, runEvent, idleEvent;
+
+  IDictionary<string, bool> animStates = new Dictionary<string, bool>();
+
+  public void Awake()
+  {
+    animStates.Add("Walk", false);
+    animStates.Add("Run", false);
+  }   
+
+  public void Update()
+  {
+    if (Input.GetKeyDown("right")){
+        animStates["Walk"] = true;
+    }
+  }
+}
