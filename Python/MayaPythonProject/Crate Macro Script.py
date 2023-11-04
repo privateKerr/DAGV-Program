4edccaa0b583bd655133b9e3e4f60056a4beba3e
Hayden Kerr <10897906@my.uvu.edu>
2023-11-03 19:01:16 -0600

    corner block and side plank additions
    Corner blocks will now scale properly with varying dimensions of the box and started to work on adding planks on the side of the crate

--- Python/MayaPythonProject/Crate Macro Script.py
+++ Python/MayaPythonProject/Crate Macro Script.py
@@ -4,6 +4,7 @@ width = 5
 height = 5
 depth = 5
 
+
 # Creates the base shape of the crate with indentations on all sides
 crate = cmds.polyCube(w=width, h=height, d=depth, name='Crate')
 inside_width = width*.25
@@ -29,5 +30,18 @@ for x in [-1, 1]:
         for z in [-1, 1]:
             corner_block = cmds.polyCube(w=(width * .22), h=(height * .22), d=(depth * .22), name="CornerBlock")
             cmds.setAttr("{}.translateX".format(corner_block[0]), x * (width * 0.45 - 0.2 * 0.45))
-            cmds.setAttr("{}.translateY".format(corner_block[0]), y * (width * 0.45 - 0.2 * 0.45))
-            cmds.setAttr("{}.translateZ".format(corner_block[0]), z * (width * 0.45 - 0.2 * 0.45))
+            cmds.setAttr("{}.translateY".format(corner_block[0]), y * (height * 0.45 - 0.2 * 0.45))
+            cmds.setAttr("{}.translateZ".format(corner_block[0]), z * (depth * 0.45 - 0.2 * 0.45))
+            cmds.setAttr("{}.scaleX".format(corner_block[0]), 1)
+            cmds.setAttr("{}.scaleZ".format(corner_block[0]), 1)
+
+# Create side planks on z-axis
+for z in [-1, 1]:
+    side_planks = [cmds.polyPlane(width=width, height=depth*0.25, sx=0, sy=0, name='Side Plank')]
+    cmds.rotate('90deg', 0, '45deg', relative=True, worldSpace=True)
+    cmds.move(0, 0, depth * 0.425)
+    cmds.polyExtrudeFacet(constructionHistory=True, keepFacesTogether=True, pivotX=0, pivotY=0, pivotZ=0,thickness=0.25)
+cmds.select(side_planks[1])
+cmds.move(0, 0, 5)
+
+
