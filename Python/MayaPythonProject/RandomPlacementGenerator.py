3b6246012b8e0f5beab5aa83e62fc0802b618dac
Hayden Kerr <10897906@my.uvu.edu>
2023-11-23 20:09:53 -0700

    RandomPlacementUI work
    Still trying to figure out how to get the UI to access the random placement function

--- Python/MayaPythonProject/RandomPlacementGenerator.py
+++ Python/MayaPythonProject/RandomPlacementGenerator.py
@@ -1,8 +1,8 @@
 import maya.cmds as cmds
 import random
 
-def random_placement(num_dups, x_min, x_max, y_min, y_max, z_min, z_max):
 
+def random_placement(num_dups, x_min, x_max, y_min, y_max, z_min, z_max):
     selection = cmds.ls(selection=True)
 
     # Runs through each selected object in the scene
@@ -20,32 +20,42 @@ def random_placement(num_dups, x_min, x_max, y_min, y_max, z_min, z_max):
             # Moves the objects
             cmds.xform(dup, worldSpace=True, translation=[rand_x, rand_y, rand_z])
 
-random_placement(200, 10, 20, 10, 30, 10, 30)
 
 class PlacementGeneratorUI:
+
     def __init__(self):
-        self.window_name = 'Generate Placement'
+        self.window_name = 'PlacementGenerator'
 
     def delete(self):
-        #Check to see if window exists and delete if true
+        # Check to see if window exists and delete if true
         if cmds.window(self.window_name, exists=True):
             cmds.deleteUI(self.window_name, window=True)
 
     def create(self):
-        #create window
+        # create window
         self.delete()
         self.window_name = cmds.window(self.window_name, title=('%s Tool' % self.window_name), resizeToFitChildren=True)
         main_column = cmds.columnLayout()
-        cmds.intFieldGrp(numberOfFields=1, label='Number of Duplicates', parent=main_column)
-        range_column = cmds.rowColumnLayout(numberOfColumns = 1)
-        cmds.intFieldGrp(numberOfFields=1, label='X Max')
-        cmds.intFieldGrp(numberOfFields=1, label='X Min')
-        cmds.intFieldGrp(numberOfFields=1, label='Y Max')
-        cmds.intFieldGrp(numberOfFields=1, label='Y Min')
-        cmds.intFieldGrp(numberOfFields=1, label='Z Max')
-        cmds.intFieldGrp(numberOfFields=1, label='Z Min')
+        dups_field = cmds.intFieldGrp(numberOfFields=1, label='Number of Duplicates', parent=main_column)
+        cmds.rowColumnLayout(numberOfColumns=1)
+        x_max_field = cmds.intFieldGrp(numberOfFields=1, label='X Max')
+        x_min_field = cmds.intFieldGrp(numberOfFields=1, label='X Min')
+        y_max_field = cmds.intFieldGrp(numberOfFields=1, label='Y Max')
+        y_min_field = cmds.intFieldGrp(numberOfFields=1, label='Y Min')
+        z_max_field = cmds.intFieldGrp(numberOfFields=1, label='Z Max')
+        z_min_field = cmds.intFieldGrp(numberOfFields=1, label='Z Min')
+
+        cmds.intFieldGrp(dups_field, query=True, value1=True)
+        cmds.intFieldGrp(x_min_field, query=True, value1=True)
+        cmds.intFieldGrp(x_max_field, query=True, value1=True)
+        cmds.intFieldGrp(y_min_field, query=True, value1=True)
+        cmds.intFieldGrp(y_max_field, query=True, value1=True)
+        cmds.intFieldGrp(z_min_field, query=True, value1=True)
+        cmds.intFieldGrp(z_max_field, query=True, value1=True)
+
         cmds.button(label='Generate Placement')
         cmds.showWindow(self.window_name)
+
 
 mainUI = PlacementGeneratorUI()
 mainUI.create()
