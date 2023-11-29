34f568f5e87ff6b213fb01de8c4156c5277555d5
Hayden Kerr <10897906@my.uvu.edu>
2023-11-28 18:32:07 -0700

    Moving Platforms
    Created platform prefabs and made sure the scripts moved them like they need. Need to fix an issue where character is not moving with the platforms as they move

--- Unity/Unit 1 Project/Assets/Scripts/Game/MovingVerticalPlatform.cs
+++ Unity/Unit 1 Project/Assets/Scripts/Game/MovingVerticalPlatform.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,31 +9,20 @@ public class MovingVerticalPlatform : MonoBehaviour
     public float distance = 4.0f; // Adjust the distance the platform travels
 
     private Vector3 startPos;
-    private Vector3 endPos;
-    private bool movingToEnd = true;
 
     private void Start()
     {
         startPos = transform.position;
-        endPos = startPos + Vector3.up * distance;
     }
 
     private void Update()
     {
-        float step = speed * Time.deltaTime;
+        // Calculate the movement using Mathf.Sin to create a smooth back-and-forth motion
+        float movement = Mathf.Sin(Time.time * speed) * distance;
 
-        if (movingToEnd)
-        {
-            transform.position = Vector3.MoveTowards(transform.position, endPos, step);
-            if (transform.position == endPos)
-                movingToEnd = false;
-        }
-        else
-        {
-            transform.position = Vector3.MoveTowards(transform.position, startPos, step);
-            if (transform.position == startPos)
-                movingToEnd = true;
-        }
+        // Move the platform horizontally
+        transform.position = startPos + new Vector3(0, movement, 0);
     }
+
 }
 
